13bbb8347530014e8435f9b964e30329aec52443
Amit Mahato <43273484+codingchat@users.noreply.github.com>
2023-08-18 03:03:30 +0530

    Update guessNumber_Game.js
    Add the new functionality that will show the message that how many times the user took to guess the right number.

--- guessNumber_Game.js
+++ guessNumber_Game.js
@@ -1,16 +1,18 @@
 let guessNumber = Math.floor(Math.random() * 100) + 1;
+let userGuess;
+let guessCount = 0; // Initialize the guess counter
 
 while (true) {
-    let userGuess = parseInt(prompt("Guess the number between 1 to 100"));
+    userGuess = parseInt(prompt("Guess the number between 1 to 100"));
+    guessCount++; // Increment the guess counter with each guess
 
     if (userGuess > guessNumber) {
-        console.log("Your number is too high", userGuess);
-    }
-    else if (userGuess < guessNumber) {
-        console.log("Your number is too low", userGuess);
-    }
-    else {
+        console.log("Your number is too high");
+    } else if (userGuess < guessNumber) {
+        console.log("Your number is too low");
+    } else {
         console.log("Congratulations! You guessed the correct number");
+        console.log("It took you " + guessCount + " guesses."); // Display the number of guesses
         break; // Exit the loop when the correct number is guessed
     }
 }
